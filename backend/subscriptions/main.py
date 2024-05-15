81e03e41ef50422ece66aafb252852e2686b04f2
BlackMonkcr <aaron.navarro@utec.edu.pe>
2024-05-15 17:56:27 -0500

    api_subscriptions

--- backend/subscriptions/main.py
+++ backend/subscriptions/main.py
@@ -1,51 +1,37 @@
 from fastapi import Depends, FastAPI, HTTPException
 import uvicorn
-from service.user_service import *
+from service.subscription_service import *
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 CORSMiddleware(app, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
-load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
-
-# METHODS FOR USERS
+@app.get("/subscriptions")
+async def get_subscriptions():
+    return getAllSubscriptions()
 
 
-@app.get("/users/")
-def read_users(id: int):
-    user = get_user_by_id_services(id)
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+@app.get("/subscriptions/{id}")
+async def get_subscription_by_id(id: int):
+    return getSubscriptionById(id)
 
-@app.get("/users/email/")
-def read_users_by_email(email: str):
-    user = get_user_by_email_services(email)
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
 
-@app.post("/users/", response_model=dict)
-def create_user(username: str, email: str, password: str):
-    create_user_service(username, email, password)
-    return {"message": "User created successfully"}
+@app.get("/subscriptions/account/{account_id}")
+async def get_subscriptions_by_account_id(account_id: int):
+    return getSubscriptionsByAccountId(account_id)
 
-@app.put("/users/", response_model=dict)
-def update_user(id: int, username: str, email: str):
-    if update_user_service(id, username, email) is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return {"message": "User updated"}
 
-@app.delete("/users/", response_model=dict)
-def delete_user(id: int):
-    if delete_user_service(id) is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return {"message": "User deleted"}
+@app.post("/subscriptions")
+async def create_subscription(account_id: int, plan: str, start_date: str, end_date: str, status: str):
+    return createSubscription(SubscriptionCreate([account_id, plan, start_date, end_date, status]))
+
+
+@app.delete("/subscriptions/{id}")
+async def delete_subscription(id: int):
+    return deleteSubscription(id)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000, proxy_headers=True, timeout_keep_alive=300)
